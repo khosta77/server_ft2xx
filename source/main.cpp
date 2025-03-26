2954c72c06ed73b88bf8b2ae23be4c43dfd89109
khosta77 <puwerfulpants@gmail.com>
2025-03-26 18:49:40 +0300

    Исправил метод

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@ int main()
     ModuleFT232RL module_;
     std::cout << module_ << std::endl;
     std::vector<uint32_t> frame = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    module_.writeData32( frame );
+    module_.writeData<uint32_t>( frame );
     //std::vector<uint32_t> retFrame( frame.size(), 0 );
     //module_.readData32( retFrame );
     auto retFrame = module_.read<uint32_t>(10000);
