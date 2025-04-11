11b68247c76aabd558b7e77c592b4be039e7f43e
khosta77 <puwerfulpants@gmail.com>
2025-04-11 14:59:54 +0300

    Что-то добавил...

--- source/ModuleStreamCore.hpp
+++ source/ModuleStreamCore.hpp
@@ -20,10 +20,26 @@ private:
     {
         while( infinit_work_ )
         {
-            std::vector<char> text = module_ft2xx_.read<char>( 5000 );
+            std::vector<char> text = module_ft2xx_.read<char>( 10000 );
+            currentTime(std::cout);
             std::cout << ">>>:" << std::string( text.begin(), text.end() ) << std::endl;
         }
     }
+
+    void currentTime( std::ostream& os )
+{
+    std::time_t now = std::time(nullptr);
+    std::tm* local_time = std::localtime(&now);
+
+    os << "["
+       << std::setfill('0') << std::setw(2) << local_time->tm_mday << "-"
+       << std::setfill('0') << std::setw(2) << (local_time->tm_mon + 1) << "-"
+       << std::setfill('0') << std::setw(4) << (local_time->tm_year + 1900) << " "
+       << std::setfill('0') << std::setw(2) << local_time->tm_hour << ":"
+       << std::setfill('0') << std::setw(2) << local_time->tm_min << ":"
+       << std::setfill('0') << std::setw(2) << local_time->tm_sec
+       << "]";
+}
 public:
 
     ModuleStreamCore( const int id = 0, const int baudrate = 9600 ) : module_ft2xx_(id, baudrate), infinit_work_(true)
@@ -71,6 +87,7 @@ public:
 
         module_ft2xx_.writeData<char>( std::vector<char>( buffer_server_.text().begin(),
                 buffer_server_.text().end() ) );
+        currentTime(std::cout);
         std::cout << "<<<:" << buffer_server_.text() << std::endl;
     }
 
